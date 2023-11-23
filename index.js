f636695dcb7e6741fe90ef7cde3e261e2305d027
Rkarthick182005 <150213464+Rkarthick182005@users.noreply.github.com>
2023-11-23 09:36:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom'
-
-import './style.css'
-import Home from './views/home'
-import NotFound from './views/not-found'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import './style.css';
+import Home from './views/home';
+import Hom from './Hom';
+import Reg from './Reg';
+import NotFound from './views/not-found';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route component={Home} exact path="/" />
-        <Route component={NotFound} path="**" />
-        <Redirect to="**" />
+        <Route path="/" exact component={Home} />
+        <Route path="/Hom" component={Hom} />
+        <Route path="/Reg" component={Reg} />
+        {/* If none of the above routes match, redirect to the not-found page */}
+        <Route path="/not-found" component={NotFound} />
+        <Redirect to="/not-found" />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
